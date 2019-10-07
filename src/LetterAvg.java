fbde8e28445de1a5c3802dfc5471359648fa96a4
Morgan Seyler <morganseyler@ou.edu>
2019-10-06 19:45:50 -0500

    Created getter,setter, and constructor for letterAverage()

--- src/LetterAvg.java
+++ src/LetterAvg.java
@@ -1,4 +1,18 @@
 
 public class LetterAvg {
 
+	private String[] nameArray = null;
+
+	public LetterAvg(char letterAverage) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String[] getNameArray() {
+		return nameArray;
+	}
+
+	public void setNameArray(String[] nameArray) {
+		this.nameArray = nameArray;
+	}
+	
 }
